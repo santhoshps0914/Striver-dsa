0ca5ac743ac49731b46735411bb6ca9caef1d5fe
santhoshkumarps <san14kumarps@gmail.com>
2026-08-22 20:26:42 +0530

    Added SumOfArrayElements

---
+++ src/basics/arrays/SumOfArrayElements.java
@@ -0,0 +1,34 @@
+package basics.arrays;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class SumOfArrayElements {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,1,1,5,1};
+
+        int sum = findSumOfArrayElements(numbers);
+
+        System.out.println("Sum of Array Elements : "+ Arrays.toString(numbers)+" is : "+sum);
+
+        System.out.println("Sum of Array Elements : "+ Arrays.toString(numbers)+" is : "+findSumOfArrayElementsByStream(numbers));
+
+    }
+
+    private static int findSumOfArrayElements(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static int findSumOfArrayElementsByStream(int[] numbers){
+
+        return Arrays.stream(numbers).sum();
+
+
+    }
+}
